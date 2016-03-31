d4915f0ac838580c47752ae407a168f98354369e
linzhangwei <lynnjadecn@gmail.com>
2016-03-31 20:36:53 +0800

    finish data read in

--- SDK-gcc-1.0/future_net/route.cpp
+++ SDK-gcc-1.0/future_net/route.cpp
@@ -1,12 +1,51 @@
 #include "route.h"
 #include "lib_record.h"
 #include <stdio.h>
+#include <string.h>
+#include <list>
+
+std::list<Edge> adj_list[MAX_POINTS];
+int required_V[MAX_POINTS] = {0};
+int start_V = -1;
+int end_V = -1;
 
 //你要完成的功能总入口
 void search_route(char *topo[5000], int edge_num, char *demand)
 {
+    read_topo(topo, edge_num);
+    read_demand(demand);
+
+
+
     unsigned short result[] = {2, 6, 3};//示例中的一个解
 
     for (int i = 0; i < 3; i++)
         record_result(result[i]);
 }
+
+void read_topo(char *topo[], int edge_num){
+    for(int i = 0; i < edge_num; i++){
+        Edge te;
+        sscanf(topo[i], "%d,%d,%d,%d", 
+           &te.index, &te.from, &te.to, &te.weight);
+        adj_list[te.from].push_back(te);
+        printf("read %d edges\n", i);
+    }
+}
+
+void read_demand(char *demand){
+    char reqs[3000];
+    sscanf(demand, "%d,%d,%s", &start_V, &end_V, reqs);
+
+    char *pch;
+    int temp;
+    memset(required_V, 0, sizeof(int) * MAX_POINTS);
+    pch = strtok(reqs, "|");
+    while(pch != NULL){
+        sscanf(pch, "%d", &temp);
+        required_V[temp] = 1;
+        pch = strtok(NULL, "|");
+    }
+}
+
+
--- SDK-gcc-1.0/future_net/route.h
+++ SDK-gcc-1.0/future_net/route.h
@@ -1,6 +1,25 @@
 #ifndef __ROUTE_H__
 #define __ROUTE_H__
 
+#define MAX_POINTS 600
+
 void search_route(char *graph[5000], int edge_num, char *condition);
+void read_topo(char *topo[], int edge_num);
+void read_demand(char *demand); 
+
+struct Edge{
+    int from;
+    int to;
+    int index;
+    int weight;
+//    Edge(){};
+//    Edge(int _from, int _to, int _index, int _weight){
+//        from = _from;
+//        to = _to;
+//        index = _index;
+//        weight = _weight;
+//    };
+//    ~Edge(){};
+};
 
 #endif
